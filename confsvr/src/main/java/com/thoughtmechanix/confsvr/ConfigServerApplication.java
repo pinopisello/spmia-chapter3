128c40edbfe8c82f4b2d2af124b16b8a73bf239d
glocon <glocon@nike>
2017-03-29 15:36:19 -0700

    update 29/3/17

--- confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -3,7 +3,6 @@ package com.thoughtmechanix.confsvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @EnableConfigServer
