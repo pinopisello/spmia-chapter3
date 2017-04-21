1a366c8d1bfef4a12846803613dee33cd968d751
glocon <glocon@nike>
2017-04-21 14:04:02 -0700

    update 4/21/17

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/repository/LicenseRepository.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/repository/LicenseRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface LicenseRepository extends CrudRepository<License,String>  {
     public List<License> findByOrganizationId(String organizationId);
     public License findByOrganizationIdAndLicenseId(String organizationId,String licenseId);
+  
 }
